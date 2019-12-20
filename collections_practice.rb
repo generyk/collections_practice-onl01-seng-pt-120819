d9576a8af7303cd56201fd39036c9e6bea38c38b
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-20 17:23:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
-sort_array_asc()
+sort_array_asc(array)
+  array.sort 
+end 
+
+
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end 
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  
+  array 
+end 
+
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+
+def 
